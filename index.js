830b7d0d30a23191611023a2662fd84be19aed31
summersampson <summersampson57@gmail.com>
2024-11-21 11:32:22 -0700

    Summer commented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 5100; //Changing this to see if this can work
+const port = 5100; //Changing this to see if this can work summer added this
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
