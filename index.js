41010db221a7b82bf23748f755a125fb511c592d
beninburley <130599399+beninburley@users.noreply.github.com>
2024-11-21 10:57:52 -0700

    Testing Change (Added comment)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 5100;
+const port = 5100; //Changing this to see if this can work
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
